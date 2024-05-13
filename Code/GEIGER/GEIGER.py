788b274f10c8338a6c4e9cec32abcc9e7d4ff3a1
balloon4computing <balloon4.computing@gmail.com>
2024-05-13 00:37:56 -0700

    Updated geiger

--- Code/GEIGER/GEIGER.py
+++ Code/GEIGER/GEIGER.py
@@ -6,18 +6,20 @@ tubeCounts = 0
 currentTime = 0
 setTime = 0
 countPerMinute = 0
+usvh_ratio = 0.00812037037037 # This is for the J305 tube
 
 # Pin configuration
-GEIGER_PIN = 4  # Assuming you're using GPIO4 for the Geiger counter; adjust as necessary
+GEIGER_PIN = 17  # Assuming you're using GPIO4 for the Geiger counter; adjust as necessary
 
 # Function to increment tubeCounts when a pulse (falling edge) is detected
 def impulse(channel):
     global tubeCounts
     tubeCounts += 1
+    print("Pulse detected. Tube counts: ", tubeCounts)
 
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)  # Use Broadcom pin-numbering scheme
-GPIO.setup(GEIGER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(GEIGER_PIN, GPIO.IN)
 
 # Add event detect for the Geiger counter pin
 GPIO.add_event_detect(GEIGER_PIN, GPIO.FALLING, callback=impulse)
@@ -35,8 +37,8 @@ def loop():
     startTime = time.time()  # Record start time
     while time.time() - startTime <= 60:  # Run loop for 60 seconds
         pass  # Wait for impulses to be counted in the background
-
-    print(tubeCounts)  # After 60 seconds, print the tubeCounts
+    print("cpm: ", tubeCounts)
+    print(tubeCounts * usvh_ratio)  # After 60 seconds, print the tubeCounts
     tubeCounts = 0  # Reset tubeCounts for the next minute
 
 if __name__ == '__main__':
