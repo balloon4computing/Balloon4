8fecd7e16f38959884d7da18f61c4ffef7a67747
Attituding <84680760+Attituding@users.noreply.github.com>
2024-07-02 20:26:41 -0700

    feat: gps logging

---
+++ Code/GPS/gpstest.py
@@ -0,0 +1,23 @@
+import sys
+import board
+import busio
+import adafruit_gps
+import serial
+# import numpy as np
+
+LOG_FILE = 'gpslog.txt'
+LOG_MODE = 'wb'
+
+# uart = busio.UART(board.TX, board.RX, baudrate = 9600, timeout = 10)
+uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=10)
+
+gps = adafruit_gps.GPS(uart)
+
+with open(LOG_FILE, LOG_MODE) as outfile:
+	while True:
+    	sentence = gps.readline()
+    	if not sentence:
+        	continue
+    	print(str(sentence,"ascii").strip())
+    	outfile.write(sentence)
+    	outfile.flush()
